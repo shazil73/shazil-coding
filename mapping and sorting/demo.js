0c503eb655b854efb0d65177f07a06552468c520
shazil73 <kaifzahidsyed@gmail.com>
2025-07-09 05:43:47 -0700

    done

---
+++ mapping and sorting/demo.js
@@ -0,0 +1,17 @@
+var hello = ['hashim','asad','zamin']
+
+hello.map((f)=> {
+    console.log("he is my best friend " + f)
+})
+
+var asia = [99, 88, 77, 66, 55, 44, 33, 22, 11]
+
+console.log(asia)
+
+asia.sort((a ,b)=> a - b)
+
+console.log(asia)
+
+asia.sort((a ,b)=>b - a)
+
+console.log(asia)
