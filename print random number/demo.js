cc9379223b5b987ec6a95475fcaf61697644582e
shazil73 <kaifzahidsyed@gmail.com>
2025-07-09 05:21:03 -0700

    done

---
+++ print random number/demo.js
@@ -0,0 +1,25 @@
+var a = 6  // number
+var b = 7.6  // number
+var c = null  // object
+var d = "Hello, World!"  //string
+var e = undefined  // undefined
+var f = true  //boolean
+var g = false  //boolean
+var h = {color: "red", size: 10, shape: "circle"}  // object
+
+console.log(typeof(e))
+
+while(a < 40) {
+    console.log(a)
+    a = a + 1
+}
+
+let i = 20
+
+const j = 30
+
+var k = 40
+
+console.log(i)
+console.log(j)
+console.log(k)
