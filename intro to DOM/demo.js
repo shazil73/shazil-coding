9cee67d707c66ac7f43958767fae0575f3594e76
shazil73 <kaifzahidsyed@gmail.com>
2025-07-09 04:47:31 -0700

    thanks

---
+++ intro to DOM/demo.js
@@ -0,0 +1,8 @@
+  var shazil = ['hashim', 'asad', 'ayyan', 'zamin', 'saad']
+ console.log(shazil.pop())
+ console.log(shazil.join('...'))
+ console.log(shazil)
+
+document.getElementsByClassName("a1")[0].innerHTML = "Hello World!"
+document.getElementsByClassName("a2")[0].innerHTML = "Hello World!"
+document.getElementsByClassName("a3")[0].innerHTML = "Hello World!"
