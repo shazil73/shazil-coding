04d88dfc74d26e702ce1e372f0b2dd72f28fe219
shazil73 <kaifzahidsyed@gmail.com>
2025-08-22 11:41:55 -0700

    gg

--- bank calculator/demo.js
+++ bank calculator/demo.js
@@ -27,5 +27,67 @@ operatorbuttons.forEach(button => {
                 operator = null;
                 updateDisplay('');
                 break;
-}})
-    })                
+            case 'backspace':
+                currentInput = currentInput.slice(0, -1);
+                updateDisplay(currentInput);
+                break;
+            case 'equals':
+                if (previousInput && currentInput && operator) {
+                    result = calculate(parseFloat(previousInput), parseFloat(currentInput), operator)
+                    updateDisplay(result.toString());
+                    currentInput = result.toString();
+                    previousInput = '';
+                    operator = null;
+                }
+                break;
+
+            case 'divide':
+            case 'multiply':
+            case 'subtract':
+            case 'sum':
+                if (currentInput) {
+                    if (previousInput && operator) {
+                        result = calculate(parseFloat(previousInput), parseFloat(currentInput), operator)
+                        updateDisplay(result.toString());
+                        previousInput = result.toString();
+                        updateDisplay(result);
+                    }
+                    else {
+                        previousInput = currentInput
+                    }
+                    currentInput = '';
+                    operator = getOperatorSymbol(id);
+                }
+                break
+
+        }
+    })
+})
+
+function getOperatorSymbol(id) {
+    switch (id) {
+        case 'divide':
+            return '/';
+        case 'multiply':
+            return '*';
+        case 'subtract':
+            return '-';
+        case 'sum':
+            return '+';
+        default:
+            return null;
+    }}
+
+function calculate(a, b, operator) {
+    switch (operator) {
+        case '/':
+            return a / b;
+        case '*':
+            return a * b;
+        case '-':
+            return a - b;
+        case '+':
+            return a + b;
+        default:
+            return null;
+    }}
